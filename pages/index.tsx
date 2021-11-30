9e98cf29b69633942c93120b86e57750e8621c2d
adheepgeorgepaul <adheepgeorgepaul@gmail.com>
2021-11-30 19:33:49 +0530

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home: FC = () => (
         <a href='https://nextjs.org'>Next.js!</a>
       </h1>
 
-            <p className={styles.description}>
+      <p className={styles.description}>
         Get started by editing{' '}
         <code className={styles.code}>pages/index.js</code>
       </p>
